b6522d54d9f6beefb7407d89c73d8bfcd6336ba8
Jalen V. Barlow <jalen.barlow@bakertilly.com>
2026-03-03 23:25:57 +0000

    Add repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,16 +8,25 @@ terraform {
     }
   }
 }
+
 provider "google" {
   # Set your GCP project ID, region, and zone here or as environment variables
   project = "project-a3c6748e-27b0-40e7-b32"
-  credentials = file("/tmp/tmp.Injr0MWqlr/application_default_credentials.json")
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  #credentials = file("/tmp/tmp.Injr0MWqlr/application_default_credentials.json")
+  region  = "us-west1"
+  zone    = "us-west1-a"
 }
+
 # Example resource: Create a VPC network
 resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
 }
 
+# Create container image repository
+resource "google_artifact_registry_repository" "jvb-repo" {
+  location      = "us-west1"
+  repository_id = "jvb-repository"
+  description   = "docker repository"
+  format        = "DOCKER"
+}
